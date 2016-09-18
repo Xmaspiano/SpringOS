7303b7a438ae4aca0d552b70f5801fee4e5d5b4f
Albert ZheYang <xmaspiano@Hotmail.com>
2016-09-18 22:33:41 +0800

    target to ignored

--- target/classes/schema/mysql/import-data.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO `NOTE_USER` VALUES (1, 'admin', 'admin', '111111', '0cafe5388f4f38a5a1e69337959772886ee32e44', 'f1522f9252635b12', CURRENT_TIMESTAMP);
-INSERT INTO `NOTE_USER` VALUES (2, 'yao', 'yao', '111111', 'd8f47269859645a153d9b7886f6bc34b39d774d7', 'b0a6d285fdb072cf', CURRENT_TIMESTAMP);
-INSERT  INTO note_department VALUES (1,'研发中心');
-INSERT  INTO note_department VALUES (2,'项目部');
-INSERT  INTO note_department VALUES (3,'市场部');
-INSERT  INTO note_department VALUES (4,'运维部');
-INSERT  INTO note_department VALUES (5,'办公室');
-INSERT INTO NOTE_BLOG VALUES (1,'《卜算子》 李之仪','一首诗歌','我住长江头，君住长江尾。 日日思君不见君，共饮长江水。
-此水几时休，此恨何时已。 只愿君心似我心，定不负相思意',1,'2016-02-02');
